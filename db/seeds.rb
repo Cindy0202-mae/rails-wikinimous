5260d69535379d8255e5abde45b6571cee8c1b6f
Cindy Mae Ngoho <cindymaen17@gmail.com>
2024-12-21 17:00:33 +0900

    7 CRUD actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,18 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Resetting fake article..."
+Article.destroy_all
+
+puts "Creating fake restaurants..."
+
+10.times do |index|
+  article = Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(sentence_count: 20)
+  )
+
+  puts "Created #{article.title}"
+end
+
+puts "Seed!"
